50ad3ac523b518ea9e6f15d97362ba9e33068976
Clayton Driver <Clayton.driver@gmail.com>
2020-06-08 21:13:13 +0000

    improved syntax

--- homework.js
+++ homework.js
@@ -6,20 +6,67 @@
 //
 // If the potential member is not in the array, return the array unchanged.
 // If the potential member is in the array, remove all instances of it from the array.
+function remove (array, potential) {
+  let newArray = array.slice();
+  for (let item of array) {
+    if (item === potential) {
 
+
+      let idx = newArray.indexOf(item)
+      newArray.splice(idx, 1);
+    }
+  }
+  return newArray;
+
+}
 // 2. Revisit your "remove" function. Make sure that it does not change the original
-// array but instead returns a new array.
+// array but instead returns a new  
 
 // 3. Create a function called "sum" that takes an array of numbers and
 // returns the sum of those numbers.
 
+// //function sum (array) {
+// let newArray = []
+// newArray = array.filter(       people => people! == potential);
+// return newArray;
+// }
+
+
+
 // 4. Create a function called "average" that takes an array of numbers
 // and returns the average of those numbers.
+
+// Function average(arr2){
+//   let average = 0;
+//   for (let i of arr2) {
+//     if (arr2.length > 0) {
+//       let average = 
+//     if (arr2.length < 0)
+//         console.log(sum)
+//     } else {
+//       returned undefined;
+//       let g = average / arr2.length;
+//       return arr2;
+//     }
 
 // 5. Create a function called "minimum" that takes an array of numbers and
 // returns the smallest number in that array.
 
-// 6. There are many techniques to sort arrays in programming. Your programming
+// function mimimum(numberArray) {
+
+//   let minimum = numberArray[0];
+//   for (let number of numberArray) {
+//     console.log(numberArray)
+//     return smallestNumber
+//     if (number <= minimum) {
+
+//       smallestNumber = number
+//     }
+//   };
+//   return smallestNumber;
+// 6. 
+
+// There are many techniques to sort arrays in programmin
 // language will likely include the ability to do this. We are going to
 // implement sorting ourselves, however.
 //
@@ -49,3 +96,7 @@
 // For example, `textList(['Cadence', 'Ordel', 'Marion'])` results in the string
 // `"Cadence,Ordel,Marion"`.
 
+// function textList(string){
+//   let newString = string.join();
+//   return newString;
+// }
